8ca7f9f787219bb1fa1bf3521aa9d5c961955744
Zaven Arra <zaven@rriv.org>
2026-08-22 23:11:54 -0700

    feat: implement manual re-run capability with date range

---
+++ pipeline/1.1-correction-pipeline-dag/2-correction-pipeline-rerun.sql
@@ -0,0 +1,21 @@
+
+-- create a table to hold configuration parameters
+CREATE OR REPLACE TABLE task_params (
+    task_name VARCHAR(36),
+    manual_data_start_at TIMESTAMP_NTZ,
+    manual_data_until TIMESTAMP_NTZ
+);
+INSERT INTO task_params (task_name, manual_data_start_at, manual_data_until) values ( 'correction_run', NULL, NULL);
+
+-- trigger into the pipeline where the configuration parameters are picked up
+
+CREATE OR REPLACE PROCEDURE run_correction(start_at TIMESTAMP, until TIMESTAMP)
+  RETURNS BOOLEAN
+  AS
+  $$
+BEGIN
+  UPDATE task_params SET manual_data_start_at = :start_at, manual_data_until = :until WHERE task_name = 'correction_run';
+  EXECUTE TASK run_correction;
+  RETURN TRUE;
+END;
+$$;
