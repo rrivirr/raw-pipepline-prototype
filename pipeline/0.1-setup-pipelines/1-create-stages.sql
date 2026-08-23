9942c96a2a618d5c54ba5698a6ea728b11ca6030
Zaven Arra <zaven@rriv.org>
2026-08-22 21:10:42 -0700

    fix: fixing dag flow

--- pipeline/0.1-setup-pipelines/1-create-stages.sql
+++ pipeline/0.1-setup-pipelines/1-create-stages.sql
@@ -1,2 +1,33 @@
 CREATE STAGE IF NOT EXISTS correction_stage
   FILE_FORMAT = (TYPE = PARQUET);
+
+
+
+CREATE OR REPLACE PROCEDURE load_from_stage (suffix VARCHAR(36), staged_file VARCHAR(36))
+  -- CREATE OR REPLACE TEMPORARY TABLE file_intake LIKE TIGERLAKE_TSDB_PUBLIC_METER;
+RETURNS STRING
+  AS
+  $$
+BEGIN
+
+let tablename := 'file_intake_' || suffix;
+
+let create_sql := '
+CREATE OR REPLACE TEMPORARY TABLE ' || tablename || ' 
+  LIKE TIGERLAKE_TSDB_PUBLIC_METER;';
+
+EXECUTE IMMEDIATE create_sql;
+
+
+let copy_sql := '
+COPY INTO ' || tablename || ' 
+  FROM @correction_stage/' || staged_file || '
+  FILE_FORMAT = (TYPE = PARQUET)
+  MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE';
+
+EXECUTE IMMEDIATE copy_sql;
+
+RETURN tablename;
+END;
+$$
+;
