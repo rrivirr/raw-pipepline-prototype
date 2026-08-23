723a4b1d1001ae6e771d93e1892760803f4286fb
Zaven Arra <zaven@rriv.org>
2026-08-22 19:27:34 -0700

    feat: correction pipeline first operational

--- pipeline/1-correction-pipeline/1-take-data.sql
+++ pipeline/1-correction-pipeline/1-take-data.sql
@@ -4,54 +4,51 @@
 -- TODO: this should be a view, not a copy
 
 CREATE OR REPLACE PROCEDURE take_data( start_at TIMESTAMP, until TIMESTAMP, lineage VARCHAR(36) )
-  RETURNS BOOLEAN
+  RETURNS STRING
   AS
   $$
+DECLARE
+  output_file STRING;
+  copy_sql STRING;
 BEGIN
 
 LET prelude_date_part := 'MINUTE';
 LET prelude_date_value := -30;
 LET prelude TIMESTAMP := DATEADD(:prelude_date_part, :prelude_date_value, start_at);
 
-CREATE OR REPLACE TABLE "1_analysis_rows" AS
-  SELECT *
+CREATE OR REPLACE TEMPORARY TABLE data_input_range AS
+  SELECT 
+    id,
+    serial_number,
+    rate,
+    length,
+    measured_at
   FROM TIGERLAKE_TSDB_PUBLIC_METER
   WHERE measured_at > :prelude
     AND measured_at < :until;
 
 -- update lineage
--- todo:  make this a function
-UPDATE lineage 
-SET attributes = OBJECT_INSERT(
-    attributes,
+CALL update_lineage(:lineage,  
     'processing_range',
-    OBJECT_CONSTRUCT('prelude', :prelude, 'start_at', :start_at, 'until', :until),
-    TRUE)
-WHERE id = :lineage;
+    OBJECT_CONSTRUCT('prelude', :prelude, 'start_at', :start_at, 'until', :until, 'processed_at', TO_VARCHAR(CURRENT_TIMESTAMP(), 'YYYYMMDD_HH24MISS'))
+  );
 
-RETURN TRUE;
+
+output_file :=  :lineage || '_' || TO_VARCHAR(CURRENT_TIMESTAMP(), 'YYYYMMDD_HH24MISS') || '_data_input_range';
+
+copy_sql := 'COPY INTO @correction_stage/' || output_file || '
+               FROM data_input_range
+               FILE_FORMAT = (TYPE = PARQUET)
+               HEADER = TRUE
+               SINGLE = TRUE
+               OVERWRITE = FALSE';
+
+EXECUTE IMMEDIATE copy_sql;
+
+
+
+
+RETURN output_file;
 END;
 $$
 ;
-
-
---  Staring on a view based system.
--- --   LET prelude_date_part := 'MINUTE';
--- --   LET prelude_date_value := -30;
--- --   LET prelude TIMESTAMP := 
-
--- CREATE OR REPLACE FUNCTION data_range(start_at TIMESTAMP, until TIMESTAMP )
--- RETURNS TABLE (serial_number STRING, rate NUMBER(12:6), measured_at TIMESTAMP)
--- LANGUAGE SQL
--- AS
--- $$
- 
-
-
---   SELECT *
---   FROM TIGERLAKE_TSDB_PUBLIC_METER
---   WHERE measured_at > DATEADD(:prelude_date_part, :prelude_date_value, start_at)
---     AND measured_at < :until
-
--- $$;
-
--- pipeline/1-correction-pipeline/3-correct-warmup.sql
+++ pipeline/1-correction-pipeline/3-correct-warmup.sql
@@ -3,18 +3,43 @@
 --- any gap beyond the threshold indicated a warmup or a potential warmup (state unknown with data outage)
 --- or flag periods with certain signature defined by the data scientist
 
-CREATE OR REPLACE PROCEDURE correct_warmup(lineage VARCHAR(36))
-  RETURNS BOOLEAN
+CREATE OR REPLACE PROCEDURE correct_warmup(lineage VARCHAR(36), staged_file VARCHAR(255))
+  RETURNS STRING
   AS
   $$
+DECLARE
+  copy_sql STRING;
+  correction_sql STRING;
+  step_name STRING;
+  output_file STRING;
 BEGIN
 
-LET warmup_reset := 10 * 60;
 
+
+LET warmup_reset := 10 * 60; -- TODO: hard coded, add to dag config and run overrides
+
+step_name := 'corrected_data_warmup';
+
+CREATE OR REPLACE TEMPORARY TABLE file_intake LIKE TIGERLAKE_TSDB_PUBLIC_METER;
+
+copy_sql := 'COPY INTO file_intake
+               FROM @correction_stage/' || staged_file || '
+               FILE_FORMAT = (TYPE = PARQUET)
+               MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE';
+
+EXECUTE IMMEDIATE copy_sql;
 
 -- TODO: this detection logic needs to be improved.  the preceeding rows count is a not a precise method
-CREATE OR REPLACE TABLE "3_correct_warmup" AS
-    SELECT *
+correction_sql := '
+CREATE OR REPLACE TABLE ' || step_name || ' AS
+    SELECT 
+        id,
+        serial_number,
+        rate,
+        length,
+        measured_at,
+        max_gap,
+        gap_length
     FROM (
         SELECT *,
         MAX(gap_length) OVER (
@@ -26,24 +51,36 @@ CREATE OR REPLACE TABLE "3_correct_warmup" AS
             SELECT *, 
             DATE_PART(EPOCH, measured_at) - LAG(DATE_PART(EPOCH, measured_at)) 
                 OVER ( PARTITION BY serial_number ORDER BY measured_at) AS gap_length
-            FROM "1_analysis_rows"
+            FROM file_intake
         )
     ) 
-    WHERE max_gap < :warmup_reset;
-  
-  
--- tag lineage here
--- lineage will be a json column where data is appended.
--- it should be one run-level lineage that travels along in the pipeline with this job.  the rows get a lineage id.
-UPDATE lineage 
-SET attributes = OBJECT_INSERT(
-    attributes,
-    'warmup_correction',
-    OBJECT_CONSTRUCT('warmup_interval', :warmup_reset),
-    TRUE)
-WHERE id = :lineage;
+    WHERE max_gap < ' || warmup_reset || ';
+';
 
-RETURN TRUE;
+EXECUTE IMMEDIATE correction_sql;
+  
+  
+-- update lineage
+CALL update_lineage(:lineage,  
+    :step_name,
+    OBJECT_CONSTRUCT('warmup_interval', :warmup_reset)
+  );
+
+
+
+output_file :=  :lineage || TO_VARCHAR(CURRENT_TIMESTAMP(), 'YYYYMMDD_HH24MISS') || '_' || :step_name;
+
+copy_sql := 'COPY INTO @correction_stage/' || output_file || '
+               FROM ' || :step_name || '
+               FILE_FORMAT = (TYPE = PARQUET)
+                HEADER = TRUE
+               SINGLE = TRUE
+               OVERWRITE = FALSE';
+
+EXECUTE IMMEDIATE copy_sql;
+
+
+RETURN output_file;
 END;
 $$
 ;
