303023f5f524f759a5b02d6f86b273dd82ac40d0
Zaven Arra <zaven@rriv.org>
2026-08-23 20:32:35 -0700

    feat: create the device lineage concept

--- pipeline/0.1-setup-pipelines/2-create-lineage-table.sql
+++ pipeline/0.1-setup-pipelines/2-create-lineage-table.sql
@@ -4,6 +4,14 @@ CREATE OR REPLACE TABLE lineage (
     attributes VARIANT DEFAULT PARSE_JSON('{}')
 );
 
+CREATE OR REPLACE TABLE device_lineage (
+    id VARCHAR(36) DEFAULT UUID_STRING() PRIMARY KEY,
+    group_lineage VARCHAR(36),
+    serial_number VARCHAR(36),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
+    attributes VARIANT DEFAULT PARSE_JSON('{}')
+);
+
 
 CREATE OR REPLACE PROCEDURE update_lineage(lineage_id VARCHAR(36), object_key VARCHAR, object_values OBJECT)
 RETURNS STRING
