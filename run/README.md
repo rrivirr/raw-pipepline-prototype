8277f3da6b6d961d981f75658a3ef6c20c5c94ca
Zaven Arra <zaven@rriv.org>
2026-08-25 02:07:31 -0700

    chore: data import working for install script

---
+++ run/README.md
@@ -0,0 +1,4 @@
+1. Copy install.env.example to install.env
+2. Populate with your snowflake credentials
+3. Run ./install.sh
+4. Use SnowSQL or SnowSight to run the commands in the project README
--- run/install.sh
+++ run/install.sh
@@ -2,7 +2,7 @@
 # Installs the correction/calibration DAG artifacts into Snowflake via SnowSQL,
 # then loads data1.csv into TIGERLAKE_TSDB_PUBLIC_METER.
 #
-# Requires ../.install.env with SnowSQL connection parameters, e.g.:
+# Requires ../install.env with SnowSQL connection parameters, e.g.:
 #   SNOWSQL_ACCOUNT=myorg-myaccount
 #   SNOWSQL_PWD=secret
 #   SNOWSQL_DATABASE=MYDB
@@ -15,7 +15,7 @@ BASE_DIR="$(dirname "$RUN_DIR")"
 LOG="$RUN_DIR/install.log"
 
 set -a
-source ".install.env"
+source "install.env"
 set +a
 
 : > "$LOG"
@@ -25,20 +25,19 @@ echo "== create-readings-table.sql" | tee -a "$LOG"
 snowsql -o exit_on_error=true -o friendly=false -f create-readings-table.sql
 
 cd data
-tar -xvzf data1.sql.tar.gz
+tar -xvzf data1.csv.tar.gz
 cd ../
 echo "== data/data1.csv -> TIGERLAKE_TSDB_PUBLIC_METER" | tee -a "$LOG"
-echo "skipping data import - needs fix"
-#snowsql -o exit_on_error=true -o friendly=false <<SQL
-#PUT file://$RUN_DIR/data/data1.csv @%TIGERLAKE_TSDB_PUBLIC_METER
-#  OVERWRITE = TRUE AUTO_COMPRESS = TRUE;
+snowsql -o exit_on_error=true -o friendly=false <<SQL
+PUT file://$RUN_DIR/data/data1.csv @%TIGERLAKE_TSDB_PUBLIC_METER
+  OVERWRITE = TRUE AUTO_COMPRESS = TRUE;
 
-#COPY INTO TIGERLAKE_TSDB_PUBLIC_METER
-#  FROM @%TIGERLAKE_TSDB_PUBLIC_METER
-#  FILE_FORMAT = (TYPE = CSV PARSE_HEADER = TRUE)
-#  MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
-#  PURGE = TRUE;
-#SQL
+COPY INTO TIGERLAKE_TSDB_PUBLIC_METER
+  FROM @%TIGERLAKE_TSDB_PUBLIC_METER
+  FILE_FORMAT = (TYPE = CSV PARSE_HEADER = TRUE)
+  MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
+  PURGE = TRUE;
+SQL
 
 echo $BASE_DIR
 for dir in 1-setup-pipelines 2-correction-pipeline-steps 3-correction-calibration-pipeline-dag; do
